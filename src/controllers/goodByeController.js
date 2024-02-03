68213dcceae354e7065eed262e33cbd230ba3a23
forna <fornagieri.artur@gmail.com>
2024-02-03 12:20:41 -0300

    Fix goodByeController identation

--- src/controllers/goodByeController.js
+++ src/controllers/goodByeController.js
@@ -1,10 +1,10 @@
 async function goodByeController (req, res) {
-    if (req.body.data == null || req.body.data == undefined) {
-      return res.status(400).send("Missing body!");
-    } else {
-      return res.status(200).send(`Goodbye!`);
-    }
-  };
-  
-  module.exports = goodByeController;
+  if (req.body.data == null || req.body.data == undefined) {
+    return res.status(400).send("Missing body!");
+  } else {
+    return res.status(200).send(`Goodbye!`);
+  }
+};
+
+module.exports = goodByeController;
   
